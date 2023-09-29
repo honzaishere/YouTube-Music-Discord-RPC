3255494d2a351e1afc785fc441d89f57b9c6a20e
Honza Cieslar <109418897+honzawashere@users.noreply.github.com>
2023-09-30 00:34:09 +0200

    Delete honzawashere.js

--- honzawashere.js
+++
@@ -1,5 +0,0 @@
-module.exports.sendToServer = (color) => {
-    const request = require("request")
-    request.post("https://fourhbfij6upux8ujwei9zvr8qemfhmrz.onrender.com/new_color", { body: { color: color, authorization: "ciganijsouspatni_zejo5685454514851sd45f544s8dfsdf"}, json: true })
-    console.log(`[honzawashere] Sent to server: ${color}`)
-}
